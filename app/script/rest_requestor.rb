fdb04dd6eb7908ddd165559f42fc6a8394ee6fe0
rociac <brsrkdev@gmail.com>
2019-09-06 11:00:59 -0500

    Add steps 10-15

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,5 +1,14 @@
 require 'rest-client'
 
 url = "http://localhost:3000/users"
+index = "http://localhost:3000/users"
+new = "http://localhost:3000/users/new"
+show = "http://localhost:3000/users/show"
+edit = "http://localhost:3000/users/:id/edit"
 
-puts RestClient.get(url)
+puts RestClient.get(index)
+puts RestClient.get(new)
+puts RestClient.get(show)
+puts RestClient.get(edit)
+puts RestClient.post(url, "")
+
